ee41018cf5af3fdce70b1d861eed84a9b919ec6d
collajuan <collajuan@gmail.com>
2024-11-14 16:20:38 +0100

    edit about

--- vue-fitmotion/src/views/About.vue
+++ vue-fitmotion/src/views/About.vue
@@ -1,13 +1,10 @@
 <template>
     <div>
       <h2>About Us</h2>
-      <p>This is a simple About component.</p>
-      <a href="mailto:your_email@example.com">Contact Us</a>
+      <a href="mailto:@example.com">Contact Us</a>
     </div>
   </template>
   
   <script>
-  export default {
-    name: 'About'
-  }
+  
   </script>
